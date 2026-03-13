f85e33f25ae797494f6c50cd52ce67c0d2900111
Lucas Gabriel <lgabriel1607@gmail.com>
2026-03-13 08:42:47 -0300

    Correção tela de tarefa

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -13,7 +13,7 @@ function TaskPage() {
 
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px] space-y-4">
+      <div className="w-[500px] mx-autospace-y-4">
         <div className="flex justify-center relative mb-6">
           <button
             onClick={onBackClick}
@@ -22,10 +22,7 @@ function TaskPage() {
             {" "}
             <ChevronLeftIcon />{" "}
           </button>
-          <Title>
-          
-            Detalhes da Tarefa
-          </Title>
+          <Title>Detalhes da Tarefa</Title>
         </div>
         <div className="bg-slate-200 p-4 rounded-md ">
           <h2 className="text-xl font-bold text-slate-600"> {title} </h2>
